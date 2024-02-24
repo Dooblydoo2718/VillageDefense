38613294c81200f7fafb8bd67211ef0f21105349
Dooblydoo2718 <161114461+Dooblydoo2718@users.noreply.github.com>
2024-02-24 15:55:50 +0800

    Delete Village Defense Remastered/save_overwriter.py

--- Village Defense Remastered/save_overwriter.py
+++
@@ -1,7 +0,0 @@
-import pickle, time
-from os import path
-from vdtoolkit import *
-text = Save(time.time())
-f2 = open(r'E:\Village Defense Remastered\GameData\Data\save.dat', 'wb')
-pickle.dump(text, f2)
-f2.close()
