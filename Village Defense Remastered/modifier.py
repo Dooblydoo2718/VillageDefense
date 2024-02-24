f182b51fe56e06b998610a9dfadf396beb117bc8
Dooblydoo2718 <161114461+Dooblydoo2718@users.noreply.github.com>
2024-02-24 15:55:35 +0800

    Delete Village Defense Remastered/modifier.py

--- Village Defense Remastered/modifier.py
+++
@@ -1,9 +0,0 @@
-import pickle
-with open(r'E:\Village Defense Remastered\GameData\Text\menu.txt', 'rb') as f:
-    d = pickle.load(f)
-# insert strange stuff here
-d['weather'] = ('Clear', 'Rainy')
-# */
-with open(r'E:\Village Defense Remastered\GameData\Text\menu.txt', 'wb') as f:
-    pickle.dump(d, f)
-    
